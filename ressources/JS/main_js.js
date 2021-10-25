58e240b44cd1517f983ba07dde12fa70e11e3d2a
Asgeir-droid <asgeir.l.aasheim@hotmail.no>
2021-10-25 11:53:34 +0200

    jk

--- ressources/JS/main_js.js
+++ ressources/JS/main_js.js
@@ -1,6 +1,33 @@
-var number = 4;
-var number2 = 18;
 
-if (number > number2) {
-	console.log("Nummer er høgare enn nummer2")
+var rightAnswer = "2469";
+
+var userAnswer = prompt("Kor høgt er Noregs høgste fjell?")
+
+//denne skjeker
+
+if (userAnswer == rightAnswer) {
+	alert("RIKTIG!")
+}
+
+
+
+var temperatur = 20;
+
+if (temperatur >= 20)
+	alert("Det har vore tropenatt")
+
+
+var nummer = 10;
+var nummer2 = "20";
+
+if (nummer == nummer2) {
+	alert("Tala er like");
+}
+
+else if (nummer < nummer2) {
+	alert("Nummer er mindre enn nummer2");
+}
+
+else {
+	alert("Tala er ulike");
 }
---
+++ ressources/JS/random_button.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+
+function displayRandomImages() 
+{
+ 
+   var imageArray = [
+   { 
+     src: "ressources/IMAGES/bulldog-1047518__340.webp",
+
+     width: "320",
+     height: "195"
+   }, 
+   {
+     src: "ressources/IMAGES/dog-4465690__340.webp",
+     width: "320",
+     height: "195"
+   }, 
+   {
+     src: "ressources/IMAGES/friends-1149841__340.webp",
+     width: "320",
+     height: "195"
+   },
+   {
+     src: "ressources/IMAGES/pug-801826_960_720.webp",
+     width: "320",
+     height: "195"
+    } ];
+    
+    
+   
+  function getRandomNum(min, max) 
+  {
+      imgNo = Math.floor(Math.random() * (max - min + 1)) + min;
+      return newArray[imgNo];
+  }  
+
+  var newImage = getRandomNum(0, newArray.length - 1);
+ 
+  var images = document.getElementsByTagName('img');
+  var l = images.length;
+  for (var p = 0; p < l; p++) {
+     images[0].parentNode.removeChild(images[0]);
+  }
+  document.body.appendChild(newImage);
+}
+
