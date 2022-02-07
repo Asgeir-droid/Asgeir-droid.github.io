d8364c364d59daec3a80aafca18b15351653de31
Asgeir-droid <asgeir.l.aasheim@hotmail.no>
2022-02-07 20:24:59 +0100

    i

---
+++ Kart/Ressursar/JS/kart.js
@@ -0,0 +1,170 @@
+//Finn ID til canvas elemetet i HTML og viser at den skal teikne i 2D
+let canvas = document.getElementById("kart");
+let ctx = canvas.getContext("2d");
+
+//Teoribygg
+ctx.moveTo(200, 5); //Seier i frå til pennen at han skal bevege seg til 200x og 5y
+ctx.lineTo(200, 220); //Drar ein strek frå der han står til 200x og 220y
+ctx.moveTo(800, 5);
+ctx.lineTo(200, 5);
+ctx.moveTo(800, 5);
+ctx.lineTo(800, 220);
+ctx.moveTo(200, 220);
+ctx.lineTo(800, 220);
+
+ctx.font = "30px Verdana"; //Viser kva font eg skal ha og kor stor han skal være
+ctx.fillText("Teoribygg", 400, 30); //Viser kva som skal stå og kvar det skal stå. Her så står det på 400x og 30y
+ctx.font = "25px Verdana";
+ctx.fillText("1.etasje", 425, 60);
+
+//Teoribygg 3D
+ctx.moveTo(200, 5);
+ctx.lineTo(175, 30);
+ctx.lineTo(175, 245);
+ctx.lineTo(200, 220);
+
+ctx.moveTo(175, 245);
+ctx.lineTo(775, 245);
+ctx.lineTo(800, 220);
+
+//2.etasje
+ctx.moveTo(1300, 5);
+ctx.lineTo(1900, 5);
+ctx.lineTo(1900, 220);
+ctx.lineTo(1300, 220);
+ctx.lineTo(1300, 5);
+
+ctx.font = "25px Verdana";
+ctx.fillText("2.etasje", 1525, 30);
+
+//2.etasje 3D
+ctx.moveTo(1300, 5);
+ctx.lineTo(1275, 30);
+ctx.lineTo(1275, 245);
+ctx.lineTo(1300, 220);
+
+ctx.moveTo(1275, 245);
+ctx.lineTo(1875, 245);
+ctx.lineTo(1900, 220);
+
+//3.etasje
+ctx.moveTo(1300, 400);
+ctx.lineTo(1900, 400);
+ctx.lineTo(1900, 625);
+ctx.lineTo(1300, 625);
+ctx.lineTo(1300, 400);
+
+ctx.font = "25px Verdana";
+ctx.fillText("3.etasje", 1525, 430);
+
+//IM
+ctx.moveTo(1415, 460);
+ctx.arc(1360, 460, 55, 0, 2 * Math.PI);
+ctx.font = "20px Verdana";
+ctx.fillText("IM", 1350, 460);
+
+//3.etasje 3D
+
+ctx.moveTo(1300, 400);
+ctx.lineTo(1275, 425);
+ctx.lineTo(1275, 650);
+ctx.lineTo(1300, 625);
+
+ctx.moveTo(1275, 650);
+ctx.lineTo(1875, 650);
+ctx.lineTo(1900, 625);
+
+//Hall 1
+ctx.moveTo(200, 400);
+ctx.lineTo(400, 400);
+ctx.moveTo(200, 400);
+ctx.lineTo(200, 500);
+ctx.lineTo(150, 500);
+ctx.lineTo(150, 1000);
+ctx.lineTo(400, 1000);
+ctx.lineTo(400,400);
+
+ctx.font = "30px Verdana";
+ctx.fillText("Hall 1", 250, 600);
+
+//Hall 1 3D
+
+ctx.moveTo(400, 400);
+ctx.lineTo(425, 425);
+ctx.lineTo(425, 1025);
+ctx.lineTo(400, 1000);
+
+ctx.moveTo(425, 1025);
+ctx.lineTo(175, 1025);
+ctx.lineTo(150, 1000);
+
+//Hall 2
+ctx.moveTo(600, 500);
+ctx.lineTo(800, 500);
+ctx.moveTo(600, 500);
+ctx.lineTo(600, 1000);
+ctx.lineTo(800, 1000);
+ctx.lineTo(800, 500);
+
+ctx.font = "30px Verdana";
+ctx.fillText("Hall 2", 650, 600);
+
+//Hall 2 3D
+ctx.moveTo(800, 500);
+ctx.lineTo(825, 525);
+ctx.lineTo(825, 1025);
+ctx.lineTo(800, 1000);
+
+ctx.moveTo(825, 1025);
+ctx.lineTo(625, 1025);
+ctx.lineTo(600, 1000);
+
+//Hall 3
+ctx.moveTo(600, 1150);
+ctx.lineTo(1000, 1150);
+ctx.moveTo(600, 1150);
+ctx.lineTo(600, 1300);
+ctx.lineTo(1000, 1300);
+ctx.lineTo(1000, 1150);
+
+//Hall 3 (3D)
+ctx.moveTo(600, 1150);
+ctx.lineTo(575, 1175);
+ctx.lineTo(575, 1325);
+ctx.lineTo(600, 1300);
+
+ctx.moveTo(575, 1325);
+ctx.lineTo(975, 1325);
+ctx.lineTo(1000, 1300);
+
+
+ctx.font = "30px Verdana";
+ctx.fillText("Hall 3", 750, 1225);
+
+//Skur
+ctx.moveTo(150, 1150);
+ctx.lineTo(300, 1150);
+ctx.moveTo(150, 1150);
+ctx.lineTo(150, 1250);
+ctx.lineTo(300, 1250);
+ctx.lineTo(300, 1150);
+
+ctx.font = "20px Verdana";
+ctx.fillText("Skur", 200, 1200);
+
+//Skur 3D
+ctx.moveTo(300, 1150);
+ctx.lineTo(325, 1165);
+ctx.lineTo(325, 1265);
+ctx.lineTo(300, 1250);
+
+ctx.moveTo(325, 1265);
+ctx.lineTo(175, 1265);
+ctx.lineTo(150, 1250);
+
+//Skille strek mellom kart og etasjer
+ctx.moveTo(1200, 0);
+ctx.lineTo(1200, 3000);
+
+//legger allt på canvasen
+ctx.stroke();
