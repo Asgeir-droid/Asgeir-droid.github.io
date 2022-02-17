6a349a758938380d309e3621f423aa6bbb5d6dce
Asgeir-droid <asgeir.l.aasheim@hotmail.no>
2022-02-17 19:20:46 +0100

    hola

--- Kart/Ressursar/JS/kart.js
+++ Kart/Ressursar/JS/kart.js
@@ -17,6 +17,13 @@ ctx.fillText("Teoribygg", 400, 30); //Viser kva som skal stå og kvar det skal s
 ctx.font = "25px Verdana";
 ctx.fillText("1.etasje", 425, 60);
 
+//Elektro vg1
+ctx.moveTo(530, 170);
+ctx.arc(480, 170, 50, 0, 2 * Math.PI);
+ctx.font = "20px Verdana";
+ctx.fillText("Elektro", 430, 175);
+ctx.fillText("vg1", 430, 190);
+
 //Teoribygg 3D
 ctx.moveTo(200, 5);
 ctx.lineTo(175, 30);
@@ -36,6 +43,12 @@ ctx.lineTo(1300, 5);
 
 ctx.font = "25px Verdana";
 ctx.fillText("2.etasje", 1525, 30);
+
+//SSR
+ctx.moveTo(1390, 170);
+ctx.arc(1350, 170, 40, 0, 2 * Math.PI);
+ctx.font = "20px Verdana";
+ctx.fillText("SSR", 1335, 175);
 
 //2.etasje 3D
 ctx.moveTo(1300, 5);
@@ -58,13 +71,12 @@ ctx.font = "25px Verdana";
 ctx.fillText("3.etasje", 1525, 430);
 
 //IM
-ctx.moveTo(1415, 460);
-ctx.arc(1360, 460, 55, 0, 2 * Math.PI);
+ctx.moveTo(1390, 450);
+ctx.arc(1350, 450, 40, 0, 2 * Math.PI);
 ctx.font = "20px Verdana";
-ctx.fillText("IM", 1350, 460);
+ctx.fillText("IMA", 1335, 455);
 
 //3.etasje 3D
-
 ctx.moveTo(1300, 400);
 ctx.lineTo(1275, 425);
 ctx.lineTo(1275, 650);
@@ -82,13 +94,44 @@ ctx.lineTo(200, 500);
 ctx.lineTo(150, 500);
 ctx.lineTo(150, 1000);
 ctx.lineTo(400, 1000);
-ctx.lineTo(400,400);
+ctx.lineTo(400, 400);
 
 ctx.font = "30px Verdana";
-ctx.fillText("Hall 1", 250, 600);
+ctx.fillText("Hall 1", 250, 375);
+
+//Automasjon
+ctx.moveTo(200, 500);
+ctx.lineTo(400, 500);
+
+ctx.font = "20px Verdana";
+ctx.fillText("Automasjon", 250, 450);
+
+//Industriteknologi
+ctx.moveTo(150, 600);
+ctx.lineTo(400, 600);
+
+ctx.font = "20px Verdana";
+ctx.fillText("Industriteknologi", 200, 550);
+
+//Bygg og anleggsteknikk
+ctx.moveTo(150, 700);
+ctx.lineTo(400, 700);
+
+ctx.font = "20px Verdana";
+ctx.fillText("Bygg- og anleggsteknikk", 150, 650);
+
+ctx.font = "20px Verdana";
+ctx.fillText("Bygg- og anleggsteknikk", 150, 900);
+
+
+//Køyretøy
+ctx.moveTo(150, 825);
+ctx.lineTo(400, 825);
+
+ctx.font = "20px Verdana";
+ctx.fillText("Køyretøy", 200, 770)
 
 //Hall 1 3D
-
 ctx.moveTo(400, 400);
 ctx.lineTo(425, 425);
 ctx.lineTo(425, 1025);
@@ -107,7 +150,26 @@ ctx.lineTo(800, 1000);
 ctx.lineTo(800, 500);
 
 ctx.font = "30px Verdana";
-ctx.fillText("Hall 2", 650, 600);
+ctx.fillText("Hall 2", 650, 475);
+
+//Elektro
+ctx.moveTo(600, 625);
+ctx.lineTo(800, 625);
+
+ctx.font = "20px Verdana";
+ctx.fillText("Elektro", 675, 575);
+
+ctx.moveTo(600, 750);
+ctx.lineTo(800, 750);
+
+ctx.font = "20px Verdana";
+ctx.fillText("Elektro", 675, 700);
+
+//Teknikk og industrielproduksjon
+ctx.font = "20px Verdana";
+ctx.fillText("Teknikk og", 600, 875);
+ctx.fillText("industrielproduksjon", 600, 900);
+
 
 //Hall 2 3D
 ctx.moveTo(800, 500);
@@ -127,6 +189,13 @@ ctx.lineTo(600, 1300);
 ctx.lineTo(1000, 1300);
 ctx.lineTo(1000, 1150);
 
+ctx.font = "30px Verdana";
+ctx.fillText("Hall 3", 750, 1125);
+
+//Grunnkompetanse
+ctx.font = "20px Verdana";
+ctx.fillText("Grunnkompetanse", 700, 1225);
+
 //Hall 3 (3D)
 ctx.moveTo(600, 1150);
 ctx.lineTo(575, 1175);
@@ -136,10 +205,6 @@ ctx.lineTo(600, 1300);
 ctx.moveTo(575, 1325);
 ctx.lineTo(975, 1325);
 ctx.lineTo(1000, 1300);
-
-
-ctx.font = "30px Verdana";
-ctx.fillText("Hall 3", 750, 1225);
 
 //Skur
 ctx.moveTo(150, 1150);
